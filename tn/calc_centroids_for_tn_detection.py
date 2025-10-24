879351fce0b1126451d4fce3f4f80f7242dd6994
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-10-25 02:23:15 +0900

    revise/scripts

--- tn/calc_centroids_for_tn_detection.py
+++ tn/calc_centroids_for_tn_detection.py
@@ -81,10 +81,10 @@ python calc_centroid.py \
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_id', type=str, help='model_path at HuggingFace Hub.')
-    parser.add_argument('TN_Type', type=str, help='Type of TN you want to detect.')
-    parser.add_argument('lang_for_TN', type=str, help='language you wan to detect as Transfer Neurons.')
-    parser.add_argument('sentence_path', type=str, help='sentences dataset path.')
+    parser.add_argument('model_id', type=str, required=True, help='model_path at HuggingFace Hub.')
+    parser.add_argument('TN_Type', type=str, required=True, help='Type of TN you want to detect.')
+    parser.add_argument('lang_for_TN', type=str, required=True, help='language you wan to detect as Transfer Neurons.')
+    parser.add_argument('sentence_path', type=str, required=True, help='sentences dataset path.')
     args = parser.parse_args()
 
     model_id = args.model_id
