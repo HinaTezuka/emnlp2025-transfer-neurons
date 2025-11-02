6597f2fc8e586719d2d980da2e13ca46782f4a4b
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-11-02 17:55:49 +0900

    revise/scripts

--- analysis/representation_similarity/hs_sim.py
+++ analysis/representation_similarity/hs_sim.py
@@ -10,7 +10,6 @@ from collections import defaultdict
 import random
 
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
